54c57ca5698547510ec22c5b4f191fd4fa781848
Sergei Zakamskov <berserg2010@gmail.com>
2022-02-21 00:17:04 +0300

    Fix publicPath

--- webpack.config.js
+++ webpack.config.js
@@ -11,118 +11,113 @@ const Dotenv = require('dotenv-webpack');
 const srcPath = path.join(__dirname, 'src');
 const distPath = path.join(__dirname, 'dist');
 
-module.exports = (env, argv) => {
-  const ASSET_PATH = argv.mode === 'production' ? '/SimpleKeyboard/' : '/';
-
-  return {
-    watchOptions: {
-      aggregateTimeout: 600,
-      poll: 1000,
-    },
-    entry: {
-      index: `${srcPath}/index.ts`,
-    },
-    mode: 'development',
-    output: {
-      path: distPath,
-      filename: '[name]-[contenthash].js',
-      publicPath: ASSET_PATH,
-    },
-    module: {
-      rules: [
-        {
-          test: /\.ts$/,
-          loader: 'ts-loader',
-          options: {
-            appendTsSuffixTo: [/\.vue$/],
+module.exports = {
+  watchOptions: {
+    aggregateTimeout: 600,
+    poll: 1000,
+  },
+  entry: {
+    index: `${srcPath}/index.ts`,
+  },
+  mode: 'development',
+  output: {
+    path: distPath,
+    filename: '[name]-[contenthash].js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        loader: 'ts-loader',
+        options: {
+          appendTsSuffixTo: [/\.vue$/],
+        },
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader',
+      },
+      // SCSS/CSS
+      {
+        test: /\.(scss|css)$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1,
+              url: false,
+              sourceMap: true,
+            },
           },
-          exclude: /node_modules/,
-        },
-        {
-          test: /\.vue$/,
-          loader: 'vue-loader',
-        },
-        // SCSS/CSS
-        {
-          test: /\.(scss|css)$/,
-          use: [
-            MiniCssExtractPlugin.loader,
-            {
-              loader: 'css-loader',
-              options: {
-                importLoaders: 1,
-                url: false,
-                sourceMap: true,
+          {
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                plugins: ['autoprefixer'],
               },
             },
-            {
-              loader: 'postcss-loader',
-              options: {
-                postcssOptions: {
-                  plugins: ['autoprefixer'],
-                },
-              },
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: true,
             },
-            {
-              loader: 'sass-loader',
-              options: {
-                sourceMap: true,
-              },
-            },
-          ],
-        },
-        // SVG
-        {
-          test: /\.svg$/,
-          loader: 'svg-sprite-loader',
-          options: {},
-        },
-      ],
-    },
-    plugins: [
-      new Dotenv(),
-      new VueLoaderPlugin(),
-      new MiniCssExtractPlugin({
-        filename: 'main-[contenthash].css',
-      }),
-      require('autoprefixer'),
-      new HtmlWebpackPlugin({
-        title: 'Simple keyboard',
-        template: `${srcPath}/index.html`,
-        filename: 'index.html',
-      }),
-      new CleanWebpackPlugin(),
-      new CopyPlugin({
-        patterns: [{ from: `${srcPath}/assets/fonts`, to: `${distPath}/fonts` }],
-      }),
-      new webpack.HotModuleReplacementPlugin(),
+          },
+        ],
+      },
+      // SVG
+      {
+        test: /\.svg$/,
+        loader: 'svg-sprite-loader',
+        options: {},
+      },
     ],
-    resolve: {
-      extensions: ['.ts', '.js', '.vue'],
-      alias: {
-        '@': srcPath,
-      },
+  },
+  plugins: [
+    new Dotenv(),
+    new VueLoaderPlugin(),
+    new MiniCssExtractPlugin({
+      filename: 'main-[contenthash].css',
+    }),
+    require('autoprefixer'),
+    new HtmlWebpackPlugin({
+      title: 'Simple keyboard',
+      template: `${srcPath}/index.html`,
+      filename: 'index.html',
+    }),
+    new CleanWebpackPlugin(),
+    new CopyPlugin({
+      patterns: [{ from: `${srcPath}/assets/fonts`, to: `${distPath}/fonts` }],
+    }),
+    new webpack.HotModuleReplacementPlugin(),
+  ],
+  resolve: {
+    extensions: ['.ts', '.js', '.vue'],
+    alias: {
+      '@': srcPath,
     },
-    optimization: {
-      minimize: true,
-      minimizer: [
-        // For webpack@5 you can use the `...` syntax to extend existing minimizers (i.e. `terser-webpack-plugin`), uncomment the next line
-        // `...`,
-        new CssMinimizerPlugin(),
-      ],
-      splitChunks: {
-        chunks: 'all',
-        name: 'vendors',
-      },
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [
+      // For webpack@5 you can use the `...` syntax to extend existing minimizers (i.e. `terser-webpack-plugin`), uncomment the next line
+      // `...`,
+      new CssMinimizerPlugin(),
+    ],
+    splitChunks: {
+      chunks: 'all',
+      name: 'vendors',
     },
-    devServer: {
-      historyApiFallback: true,
-      open: true,
-      compress: true,
-      client: {
-        overlay: true,
-      },
-      port: 8888,
+  },
+  devServer: {
+    historyApiFallback: true,
+    open: true,
+    compress: true,
+    client: {
+      overlay: true,
     },
-  };
+    port: 8888,
+  },
 };
